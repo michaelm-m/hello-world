8a5771258d93bd93d022a40d1855944271498ab6
Michael <mcloughlin34@hotmail.com>
2018-12-20 23:01:58 -0500

    new

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 //importing node framework
 var express = require('express');
- 
+var path = require('path');
+
 var app = express();
-//Respond with "hello world" for requests that hit our root "/"
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
+ res.render('index', {});
+});
+
+app.get('/hello', function (req, res) {
  res.send('hello there');
 });
+
+
+function checkRange(x, a, b) {
+  return a<x==x<b;
+}
+
+console.log('Range : ', checkRange(15, 20, 10));
+
+
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
+app.listen(3000);
+console.log('Running on Port : 3000');
+
 module.exports = app;
